ab21debefc1a363115f3c31048245e1a806c5d4d
andres-cuenta <52874360+andres-cuenta@users.noreply.github.com>
2021-07-04 17:51:40 +1000

    [js, css, svg] Delete auto-generated files inside src folder/created index/App js files and new Accordion component js file

--- reactjs/widgets/src/App.js
+++ reactjs/widgets/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Accordion from './components/Accordion';
 
-export default App;
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a front end javascript framework'
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is a favourtie JS library among engineers'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use React by creating components'
+    },
+]
+
+const widget = () => {
+    return (
+        <Accordion  items={items} />
+    );
+};
+
+export default widget;
