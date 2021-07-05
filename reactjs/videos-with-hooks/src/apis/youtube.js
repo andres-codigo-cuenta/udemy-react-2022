a33a389c6cb33af6814c671acccf40739b83f8ea
andres-cuenta <52874360+andres-cuenta@users.noreply.github.com>
2021-07-06 09:26:56 +1000

    [.env, js] created .env file/updated gitignore to not track changes/moved api key from youtube.js into env file and updated const key

--- reactjs/videos-with-hooks/src/apis/youtube.js
+++ reactjs/videos-with-hooks/src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyBCQeFr2GvKrRmHDpmg0JFU1FRnHpw638g';
+const KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
