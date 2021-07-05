012bca76464f9f9d0918a123785a1306ed57fe65
andres-cuenta <52874360+andres-cuenta@users.noreply.github.com>
2021-07-05 17:22:17 +1000

    Custom Hook function created and added to App

--- reactjs/videos-with-hooks/src/components/App.js
+++ reactjs/videos-with-hooks/src/components/App.js
@@ -1,43 +1,31 @@
 import React, { useState, useEffect } from 'react';
 
 import SearchBar from './SearchBar/SearchBar';
-import youtube from '../apis/youtube';
 import VideoList from './VideoList/VideoList';
 import VideoDetail from './VideoDetail/VideoDetail';
+import useVideos from '../hooks/useVideos';
 
 const App = () => {
-    const [videos, setVideos] = useState([]);
     const [selectedVideo, setSelectedVideo] = useState(null);
+    const [videos, search] = useVideos('copa america');
 
     useEffect(() => {
-        onTermSubmit('copa america');
-    }, []);
-
-    const onTermSubmit = async (term) => {
-        const response = await youtube.get('/search', {
-            params: {
-                q: term
-            }
-        });
-
-        setVideos(response.data.items);
-        setSelectedVideo(response.data.items[0]);
-    };
-
-    const onVideoSelect = (video) => {
-        setSelectedVideo(video);
-    };
+        setSelectedVideo(videos[0]);
+    }, [videos]);
 
     return(
         <div className="ui container">
-            <SearchBar onFormSubmit={onTermSubmit} />
+            <SearchBar onFormSubmit={search} />
             <div className="ui grid">
                 <div className="ui row">
                     <div className="eleven wide column">
                         <VideoDetail video={selectedVideo} />
                     </div>
                     <div className="five wide column">
-                        <VideoList onVideoSelect={onVideoSelect} videos={videos} />
+                        <VideoList
+                            onVideoSelect={setSelectedVideo}
+                            videos={videos}
+                        />
                     </div>
                 </div>
             </div>
