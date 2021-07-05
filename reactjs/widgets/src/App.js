c5304e728372d9079cf2c6f290392662dec9cfce
andres-cuenta <52874360+andres-cuenta@users.noreply.github.com>
2021-07-05 16:22:04 +1000

    Header links routing for components without using React Router

--- reactjs/widgets/src/App.js
+++ reactjs/widgets/src/App.js
@@ -1,51 +1,62 @@
 import React, { useState } from 'react';
 
-// import Accordion from './components/Accordion';
-// import Search from './components/Search';
-// import Dropdown from './components/Dropdown';
+import Route from './components/Route';
+
+import Header from './components/Header';
+
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
 
-// const items = [
-// 	{
-// 		title: 'What is React?',
-// 		content: 'React is a front end javascript framework'
-// 	},
-// 	{
-// 		title: 'Why use React?',
-// 		content: 'React is a favourtie JS library among engineers'
-// 	},
-// 	{
-// 		title: 'How do you use React?',
-// 		content: 'You use React by creating components'
-// 	},
-// ]
+const items = [
+	{
+		title: 'What is React?',
+		content: 'React is a front end javascript framework'
+	},
+	{
+		title: 'Why use React?',
+		content: 'React is a favourtie JS library among engineers'
+	},
+	{
+		title: 'How do you use React?',
+		content: 'You use React by creating components'
+	},
+];
 
-// const options = [
-// 	{
-// 		label: 'The Colour Red',
-// 		value: 'red'
-// 	},
-// 	{
-// 		label: 'The Colour Green',
-// 		value: 'green'
-// 	},
-// 	{
-// 		label: 'A Shade of Blue',
-// 		value: 'blue'
-// 	},
-// ];
+const options = [
+	{
+		label: 'The Colour Red',
+		value: 'red'
+	},
+	{
+		label: 'The Colour Green',
+		value: 'green'
+	},
+	{
+		label: 'A Shade of Blue',
+		value: 'blue'
+	},
+];
 
 const Widget = () => {
+	const [selected, setSelected] = useState(options[0]);
+
 	return (
 		<div>
-			{/* <Accordion items={items} /> */}
-			{/* <Search /> */}
-			{/* <Dropdown
-				selected={selected}
-				onSelectedChange={setSelected}
-				options={options}
-			/> */}
-			<Translate />
+			<Header />
+			<Route path="/">
+				<Accordion items={items} />
+			</Route>
+			<Route path="/list">
+				<Search />
+			</Route>
+			<Route path="/dropdown">
+				<Dropdown label="Select a color" options={options} selected={selected} onSelectedChange={setSelected} />
+			</Route>
+			<Route path="/translate">
+				<Translate />
+			</Route>
 		</div>
 	);
 };
