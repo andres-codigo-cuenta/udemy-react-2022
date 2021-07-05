57047dbcb8721b81ef21924646da8cbdefddfb92
andres-cuenta <52874360+andres-cuenta@users.noreply.github.com>
2021-07-05 14:32:40 +1000

    [js] Convert and Translate components created/google translate api used to translate user text into language selected from dropdown list

--- reactjs/widgets/src/App.js
+++ reactjs/widgets/src/App.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 
 // import Accordion from './components/Accordion';
 // import Search from './components/Search';
-import Dropdown from './components/Dropdown';
+// import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
 
 // const items = [
 // 	{
@@ -19,37 +20,32 @@ import Dropdown from './components/Dropdown';
 // 	},
 // ]
 
-const options = [
-	{
-		label: 'The Colour Red',
-		value: 'red'
-	},
-	{
-		label: 'The Colour Green',
-		value: 'green'
-	},
-	{
-		label: 'A Shade of Blue',
-		value: 'blue'
-	},
-];
+// const options = [
+// 	{
+// 		label: 'The Colour Red',
+// 		value: 'red'
+// 	},
+// 	{
+// 		label: 'The Colour Green',
+// 		value: 'green'
+// 	},
+// 	{
+// 		label: 'A Shade of Blue',
+// 		value: 'blue'
+// 	},
+// ];
 
 const Widget = () => {
-	const [selected, setSelected] = useState(options[0]);
-	const [showDropdown, setShowDropdown] = useState(true);
-
 	return (
 		<div>
 			{/* <Accordion items={items} /> */}
 			{/* <Search /> */}
-			<button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
-			{showDropdown ?
-				<Dropdown
+			{/* <Dropdown
 				selected={selected}
 				onSelectedChange={setSelected}
 				options={options}
-			/> : null
-			}
+			/> */}
+			<Translate />
 		</div>
 	);
 };
