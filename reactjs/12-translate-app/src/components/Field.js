56b4fa22b4754f7f4e5df7319e6f71d24ba1a117
andres-cuenta <52874360+andres-cuenta@users.noreply.github.com>
2021-10-04 16:31:21 +1100

    [js] Convert redux form to Context

--- reactjs/12-translate-app/src/components/Field.js
+++ reactjs/12-translate-app/src/components/Field.js
@@ -5,7 +5,7 @@ class Field extends React.Component {
 	static contextType = LanguageContext;
 
 	render() {
-		const text = this.context === 'english' ? 'Name' : 'Název';
+		const text = this.context.language === 'english' ? 'Name' : 'Název';
 
 		return (
 			<div className="ui field">
