df9b417d3b41807baf6231b818092fd5230f1810
andres-cuenta <52874360+andres-cuenta@users.noreply.github.com>
2021-10-28 12:56:21 +1100

    [npm, js] youtube-api-search package added/component files created/basic input jsx added to search_bar js file

--- reactjs/13-redux-simple-starter/src/index.js
+++ reactjs/13-redux-simple-starter/src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBCQeFr2GvKrRmHDpmg0JFU1FRnHpw638g';
+
 // Create a new component.  This component should produce some HTML
 if (module.hot) {
 	module.hot.accept();
 }
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+  	<div>
+		  <SearchBar />
+	</div>
+  );
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
