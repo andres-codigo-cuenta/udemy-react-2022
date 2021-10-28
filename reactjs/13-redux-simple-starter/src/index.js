fb6d6512af693f647318d00dd49cb8bbf493f498
andres-cuenta <52874360+andres-cuenta@users.noreply.github.com>
2021-10-28 15:59:56 +1100

    [npm, js, css] lodash package added/video list, item and details components created/bootstra cdn added and style css file w css declarations

--- reactjs/13-redux-simple-starter/src/index.js
+++ reactjs/13-redux-simple-starter/src/index.js
@@ -1,24 +1,56 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyBCQeFr2GvKrRmHDpmg0JFU1FRnHpw638g';
 
-// Create a new component.  This component should produce some HTML
 if (module.hot) {
 	module.hot.accept();
 }
+class App extends Component {
+	constructor(props) {
+		super(props);
 
-const App = () => {
-  return (
-  	<div>
-		  <SearchBar />
-	</div>
-  );
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
+
+		this.videoSearch('surfboards');
+	}
+
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term: term}, videos => {
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			});
+		});
+	}
+
+	render() {
+		const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
+		return (
+			<div className="container">
+				<SearchBar onSearchTermChange={videoSearch} />
+				<div className="row">
+					<VideoDetail video={this.state.selectedVideo} />
+					<VideoList
+						onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+						videos={this.state.videos}
+					/>
+				</div>
+			</div>
+		);
+	}
 }
 
-// Take this component's generated HTML and put it on the page (in the DOM)
 ReactDOM.render(
 	<App />, document.querySelector('#root')
 );
